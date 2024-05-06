98f837b35003995b3013546c22c91ebff692040f
ReV3rb698 <xetron34@gmail.com>
2024-05-06 16:09:12 -0700

    merging branch update

--- config/database.js
+++ config/database.js
@@ -1,13 +1,13 @@
-const mysql = require('mysql2');
+const mysql = require('mysql2'); // Import the mysql2 module
 const fs = require('fs');
 const path = require('path');
-const dotenv = require('dotenv');
-dotenv.config();
+const dotenv = require('dotenv'); // Import the dotenv module
+dotenv.config(); // Configure dotenv to read the .env file
 
-const sslCertPath = path.join(__dirname, process.env.SSL_CERT_PATH);
+const sslCertPath = path.join(__dirname, process.env.SSL_CERT_PATH); // Get the SSL certificate path from the .env file
 
 let sslOptions = {};
-try {
+try { // Try to load the SSL certificate from the path
     sslOptions = {
         ca: fs.readFileSync(sslCertPath)
     };
@@ -16,7 +16,7 @@ try {
     process.exit(1);  // Terminate the application if SSL configuration fails
 }
 
-const userPool = mysql.createPool({
+const userPool = mysql.createPool({ // Create a connection pool to the MySQL database using the mysql2 module
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     database: process.env.DB_DATABASE,
@@ -25,6 +25,6 @@ const userPool = mysql.createPool({
     ssl: sslOptions
 });
 
-const promiseUserPool = userPool.promise();
+const promiseUserPool = userPool.promise(); // Create a promise pool from the connection pool
 
-module.exports = { promiseUserPool };
+module.exports = { promiseUserPool }; // Export the promise pool for use in other modules
